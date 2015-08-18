d2413cc52970853c5c172c22716f53a2455bfe1a
liuin <20893791@qq.com>
2015-08-18 17:21:02 +0800

    0818

---
+++ pro-detail.php
@@ -0,0 +1,58 @@
+<?php $page='产品内页'; include('inc/header.php'); ?>
+
+  <div class="main-box container">
+    <?php include('inc/nav-pills.php'); ?>      
+    <div class="pro-detail-box box1 row">
+      <div class="col-sm-6">
+        
+      </div>
+      <div class="pro-detail-info col-sm-6">
+        <h3>[北京路] 广州宾馆得云宫酒楼</h3>
+        <p class="p1">代金券，可叠加，不限时段通用！除茶位、烟酒、特价菜、宴席、海鲜以及餐饮服务费外全场通用，包厢可用，免费WiFi，首次开团，独家合作！</p>
+        <div class="price">
+          ¥<span class="sp1">85</span>
+        </div>
+        <div class="pj row">
+          <div class="col-xs-4">已售473份</div>
+          <div class="col-xs-4"> 4.0 27 条团购评价</div>
+          <div class="col-xs-4">
+            <p class="sup1">支持随时退</p>
+            <p class="sup1">支持过期退</p>
+          </div>
+        </div>
+        <div class="effect-date">
+          有效期：至2016-3-31
+        </div>
+        <div class="count">
+          数量：<a class="c-sub" href="#">-</a> <input type="text" name="" id="" /> <a href="#" class="c-add">+</a>
+        </div>
+        <div class="cart-bk">
+          <button class="btn btn-primary">立即抢购</button>   
+          <button class="btn btn-danger">添加到购物车</button>   
+        </div>
+      </div>
+    </div>
+
+    <div class="pro-detail-box2 box1 row">
+      <div class="col-sm-9 pro-detail-box2-info">
+        <div class="pro-detail-tag">
+          <a href="#">门店地图</a>
+          <a href="#">团购详情</a>
+          <a href="#">购买须知</a>
+          <a href="#">会员评价</a>
+        </div>
+        <div class="pro-detail-tag-section">
+        
+        </div>
+
+      </div>
+      <div class="col-sm-3"></div>
+    </div>
+    
+      
+
+
+  </div>
+  <!-- /中间 -->
+
+  <?php include('inc/footer.php'); ?>
